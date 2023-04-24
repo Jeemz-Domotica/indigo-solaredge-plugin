8b1462edec768435fc7d088219753beb893c8d48
SarahEmaAllam <s.allam@student.rug.nl>
2023-04-24 14:38:05 +0200

    sitenergy updated

--- Contents/Server Plugin/siteEnergy.py
+++ Contents/Server Plugin/siteEnergy.py
@@ -7,7 +7,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import os
-
+from datetime import datetime
 
 print(sys.argv)
 endpoint = sys.argv[1]
@@ -20,59 +20,63 @@ consumption = []
 production = []
 #fig, ax = plt.subplots(len(newData))
 types={}
-for idx, telemetry in enumerate(newData):
-    type = telemetry.get("type").encode('utf-8')
-    print("type : ", type)
-    types[type] = {}
-    types[type]['dates'] = []
-    types[type]['values'] = []
-    for meterTelemetry in telemetry.get("values"):
-        if type == "FeedIn":
-            print("meterTelemetry: ", meterTelemetry)
-        date = meterTelemetry.get("date")
+try :
+    for idx, telemetry in enumerate(newData):
+        type = telemetry.get("type").encode('utf-8')
+        print("type : ", type)
+        types[type] = {}
+        types[type]['dates'] = []
+        types[type]['values'] = []
+        for meterTelemetry in telemetry.get("values"):
+            if type == "FeedIn":
+                print("meterTelemetry: ", meterTelemetry)
+            date = meterTelemetry.get("date")
+            date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 
-        value = meterTelemetry.get("value")
-        if value != None and date != None:
-            types[type]['dates'].append(date)
-            types[type]['values'].append(value)
-
-
-            #ax[idx].plot(date, value)
-# print(types)
-print(types['FeedIn'])
-print(len(types.keys()))
-print(types.keys())
-
-fig, ax = plt.subplots(len(types.keys()))
-idx = 0
-for type in types.keys():
-    print("key: " + type)
-    ax[idx].plot(types[type].get('dates'), types[type].get('values'))
-    ax[idx].set_xticks([ax[idx].get_xticks()[0], ax[idx].get_xticks()[-1]])
-    ax[idx].set_yticks([ax[idx].get_yticks()[0], ax[idx].get_yticks()[-1]])
-    ax[idx].set_ylabel(type)
-    idx = idx + 1
-
-    # ax[idx].set_xticks([ax[idx].get_xticks()[0], ax[idx].get_xticks()[-1]])
-    # ax[idx].set_yticks([ax[idx].get_yticks()[0], ax[idx].get_yticks()[-1]])
-    # ax[idx].set_ylabel(type)
-    # plt.show()
+            value = meterTelemetry.get("value")
+            if value != None and date != None:
+                types[type]['dates'].append(date)
+                types[type]['values'].append(value)
 
 
-fig.tight_layout()
-pwd = os.path.join("Library", "Application Support", "Perceptive Automation", "Indigo 2022.2", "Web Assets", "images")
-print(os.getcwd())
-os.chdir('..')
-os.chdir('..')
-os.chdir('..')
-os.chdir('..')
-print(os.getcwd())
-os.chdir('Web Assets')
-print(os.getcwd())
-os.chdir('images')
-print(os.getcwd())
-os.chdir('controls')
-os.chdir('static')
-print(os.getcwd())
-plt.savefig(os.path.join(os.getcwd(), "energy-"+inverter_serialNumber+".png"))
+                #ax[idx].plot(date, value)
+    # print(types)
+    print(types['FeedIn'])
+    print(len(types.keys()))
+    print(types.keys())
 
+    fig, ax = plt.subplots(len(types.keys()))
+    idx = 0
+    for type in types.keys():
+        print("key: " + type)
+        ax[idx].plot(types[type].get('dates'), types[type].get('values'))
+        ax[idx].set_xticks([ax[idx].get_xticks()[0], ax[idx].get_xticks()[-1]])
+        ax[idx].set_yticks([ax[idx].get_yticks()[0], ax[idx].get_yticks()[-1]])
+        ax[idx].set_ylabel(type)
+        idx = idx + 1
+
+        # ax[idx].set_xticks([ax[idx].get_xticks()[0], ax[idx].get_xticks()[-1]])
+        # ax[idx].set_yticks([ax[idx].get_yticks()[0], ax[idx].get_yticks()[-1]])
+        # ax[idx].set_ylabel(type)
+        # plt.show()
+
+
+    fig.tight_layout()
+    pwd = os.path.join("Library", "Application Support", "Perceptive Automation", "Indigo 2022.2", "Web Assets", "images")
+    print(os.getcwd())
+    os.chdir('..')
+    os.chdir('..')
+    os.chdir('..')
+    os.chdir('..')
+    print(os.getcwd())
+    os.chdir('Web Assets')
+    print(os.getcwd())
+    os.chdir('images')
+    print(os.getcwd())
+    os.chdir('controls')
+    os.chdir('static')
+    print(os.getcwd())
+    plt.savefig(os.path.join(os.getcwd(), "energy-"+inverter_serialNumber+".png"))
+
+except Exception as e:
+    print(e)
--- Contents/Server Plugin/sitePowerFlow.py
+++ Contents/Server Plugin/sitePowerFlow.py
@@ -6,62 +6,67 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import os
 
-import datetime
+from datetime import datetime
 print(sys.argv)
 endpoint = sys.argv[1]
 inverter_serialNumber = sys.argv[2]
 response = requests.get(endpoint)
 data = response.json()
-print(data)
 newData = data["data"]["telemetries"]
 powers = []
 dates = []
 voltages = []
 temperatures = []
 energies = []
-for telemetry in newData:
-    date = telemetry.get("date")
 
-    totalActivePower = telemetry.get("totalActivePower")
-    powers.append(float(totalActivePower))
-    dates.append(date)
+try:
+    for telemetry in newData:
+        date = telemetry.get("date")
 
-    dcVoltage = telemetry.get("dcVoltage")
-    voltages.append(dcVoltage)
+        date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 
-    totalEnergy = format(telemetry.get("totalEnergy"), '.2f')
-    energies.append(totalEnergy)
-    temperature = telemetry.get("temperature")
-    temperatures.append(temperature)
+        totalActivePower = telemetry.get("totalActivePower")
+        powers.append(float(totalActivePower))
+        dates.append(date)
 
-fig, ax = plt.subplots(4)
-ax[0].plot(dates, powers)
-ax[0].set_xticks([ax[0].get_xticks()[0], ax[0].get_xticks()[-1]])
-ax[0].set_ylabel('totalActivePower')
-ax[1].plot(dates, voltages)
-ax[1].set_xticks([ax[1].get_xticks()[0], ax[1].get_xticks()[-1]])
-ax[1].set_ylabel('dcVoltage')
-ax[2].plot(dates, energies)
-ax[2].set_xticks([ax[2].get_xticks()[0], ax[2].get_xticks()[-1]])
-ax[2].set_yticks([ax[2].get_yticks()[0], ax[2].get_yticks()[-1]])
-ax[2].set_ylabel('totalEnergy')
-ax[3].plot(dates, temperatures)
-ax[3].set_ylabel('Temperature')
-ax[3].set_xticks([ax[3].get_xticks()[0], ax[3].get_xticks()[-1]])
-fig.tight_layout()
-pwd = os.path.join("Library", "Application Support", "Perceptive Automation", "Indigo 2022.2", "Web Assets", "images")
-print(os.getcwd())
-os.chdir('..')
-os.chdir('..')
-os.chdir('..')
-os.chdir('..')
-print(os.getcwd())
-os.chdir('Web Assets')
-print(os.getcwd())
-os.chdir('images')
-print(os.getcwd())
-os.chdir('controls')
-os.chdir('static')
-print(os.getcwd())
-plt.savefig(os.path.join(os.getcwd(), "power-"+inverter_serialNumber+".png"))
+        dcVoltage = telemetry.get("dcVoltage")
+        voltages.append(dcVoltage)
+
+        totalEnergy = format(telemetry.get("totalEnergy"), '.2f')
+        energies.append(totalEnergy)
+        temperature = telemetry.get("temperature")
+        temperatures.append(temperature)
+    print("plotting")
+
+    fig, ax = plt.subplots(4)
+    ax[0].plot_date(dates, powers)
+    ax[0].set_xticks([ax[0].get_xticks()[0], ax[0].get_xticks()[-1]])
+    ax[0].set_yticks([ax[0].get_yticks()[0], ax[0].get_yticks()[-1]])
+    ax[0].set_ylabel('totalActivePower')
+    ax[1].plot_date(dates, voltages)
+    ax[1].set_xticks([ax[1].get_xticks()[0], ax[1].get_xticks()[-1]])
+    ax[1].set_yticks([ax[1].get_yticks()[0], ax[1].get_yticks()[-1]])
+    ax[1].set_ylabel('dcVoltage')
+    ax[2].plot_date(dates, energies)
+    ax[2].set_xticks([ax[2].get_xticks()[0], ax[2].get_xticks()[-1]])
+    ax[2].set_yticks([ax[2].get_yticks()[0], ax[2].get_yticks()[-1]])
+    ax[2].set_ylabel('totalEnergy')
+    ax[3].plot_date(dates, temperatures)
+    ax[3].set_ylabel('Temperature')
+    ax[3].set_xticks([ax[3].get_xticks()[0], ax[3].get_xticks()[-1]])
+    ax[3].set_yticks([ax[3].get_yticks()[0], ax[3].get_yticks()[-1]])
+    fig.tight_layout()
+    pwd = os.path.join("Library", "Application Support", "Perceptive Automation", "Indigo 2022.2", "Web Assets", "images")
+    os.chdir('..')
+    os.chdir('..')
+    os.chdir('..')
+    os.chdir('..')
+    os.chdir('Web Assets')
+    os.chdir('images')
+    os.chdir('controls')
+    os.chdir('static')
+    print(os.getcwd())
+    plt.savefig(os.path.join(os.getcwd(), "power-"+inverter_serialNumber+".png"))
+except Exception as e:
+    print(e)
 
